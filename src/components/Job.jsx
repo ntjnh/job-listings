10ec8e9523f49b785b29f8be2e267413b776e609
Nate <codebynate@gmail.com>
2025-01-08 20:13:15 +0000

    Job tablet and desktop styling

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -3,20 +3,25 @@ import React from 'react'
 const Job = ({ id, logo, company, position, postedAt, contract, location, role, level, languages, tools, isNew, isFeatured }) => {
     return (
         <article className={`job${isNew ? ' new': ''}${isFeatured ? ' featured': ''}`}>
-            <img className="logo" src={logo} alt={company} />
-            <h3 className="company">
-                {company}
-                {isNew && <span className="badge badge--new">New!</span>}
-                {isFeatured && <span className="badge badge--featured">Featured</span>}
-            </h3>
-            <h2 className="position">{position}</h2>
-            <p className="info">
-                <span className="posted-at">{postedAt}</span>
-                <span className="separator">•</span>
-                <span className="contract">{contract}</span>
-                <span className="separator">•</span>
-                <span className="location">{location}</span>
-            </p>
+            <div className="details-wrapper">
+                <img className="logo" src={logo} alt={company} />
+
+                <div>
+                    <h3 className="company">
+                        {company}
+                        {isNew && <span className="badge badge--new">New!</span>}
+                        {isFeatured && <span className="badge badge--featured">Featured</span>}
+                    </h3>
+                    <h2 className="position">{position}</h2>
+                    <p className="info">
+                        <span className="posted-at">{postedAt}</span>
+                        <span className="separator">•</span>
+                        <span className="contract">{contract}</span>
+                        <span className="separator">•</span>
+                        <span className="location">{location}</span>
+                    </p>
+                </div>
+            </div>
 
             <ul className="tags">
                 <li className="tag role">{role}</li>
